eaf1ae93d495cd0e91eb0a272a42f774f79396a3
roodhouse <rughjm@gmail.com>
2023-06-29 10:49:27 -0500

    body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Body() {
   return (
-    <div>Body</div>
+    <>
+      <div id="bodyContainer">
+        <div id="heading" className='text-gray text-[22px] leading-[32px] font-light text-center font-["Libre_Franklin"] pb-[15px]'>
+          <h1>We are launching <span className='text-darkBlue font-bold'>soon!</span></h1>
+        </div>
+        <div id="text" className='text-darkBlue text-xs font-light text-center font-["Libre_Franklin"]'>
+          <p>Subscribe and get notified</p>
+        </div>
+      </div>
+    </>
   )
 }
 
