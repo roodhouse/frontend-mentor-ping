903078fbd4773e9aa464ff3be9957f0efbbaad46
roodhouse <rughjm@gmail.com>
2023-06-29 10:43:41 -0500

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Logo from '../images/logo.svg'
 function Header() {
   return (
-    <div>Header</div>
+    <>
+      <div id="headerContainer" className='flex justify-center'>
+        <img src={Logo} alt="Logo" />
+      </div>
+    </>
   )
 }
 
