64bb1c28a054716ad40c0cb66cffa777fb695965
roodhouse <rughjm@gmail.com>
2023-06-29 11:20:33 -0500

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Form() {
   return (
-    <div>Form</div>
+    <>
+      <div id="formContainer" className='px-[20px]'>
+        <form className='flex flex-col items-center justify-center'>
+          <div id="emailContainer" className='py-[10px] pl-8 mb-[10px] rounded-[28px] border border-solid border-formBorder shadow-[0px_0px_7px_3px_rgba(0,0,0,0.00)] w-full'>
+            <input type="email" id='email' name='email' placeholder='Your email address...' className='placeholder:font-["Libre_Franklin"] placeholder:text-placeHolder placeholder:text-xs placeholder:leading-[20px] placeholder:font-light' />
+          </div>
+          <div id="submitContainer" className='cursor-pointer rounded-[28px] bg-blue shadow-[0px_5px_10px_2px_rgba(76,123,243,0.23)] text-white w-full text-center text-xs font-["Libre_Franklin"] font-semibold leading-[16px]'>
+            <input type="submit" id="submit" name='submit' value="Notify Me" className='py-[15px] w-full'/>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
